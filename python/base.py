90548f315cd6f24ae7516937ef31accfd24a0578
James Dang <dangjc@yahoo.com>
2017-12-04 22:04:25 -0800

    drop unused kernels

--- python/base.py
+++ python/base.py
@@ -2,13 +2,6 @@ import tensorflow as tf
 
 lng_kernels = tf.load_op_library('libLatticeCtc.so')
 
-# kernels for testing
-zero_out_kernel = lng_kernels.zero_out
-add_one_kernel = lng_kernels.add_one
-mul_by_two_kernel = lng_kernels.mul_by_two
-jagged_edit_kernel = lng_kernels.jagged_edit
-pass_array_kernel = lng_kernels.pass_array
-
 hmm_forward_kernel = lng_kernels.hmm_forward
 hmm_backward_kernel = lng_kernels.hmm_backward
 hmm_viterbi_kernel = lng_kernels.hmm_viterbi
